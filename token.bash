8f94e1296fa4b3132fa11103ce77eb61b330f93a
hatake <hermit183@outlook.com>
2025-06-17 14:47:13 +0900

    add setting path process

--- token.bash
+++ token.bash
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+path=""
+
+if [ $# -eq 0 ]; then
+	for (( i=0; true; i++ )); do
+		path="untitled-$i.txt"
+		if [ ! -f $path ]; then
+			break
+		fi
+	done
+else
+	path=$1
+fi
+
 cmd=""
 
 while true; do
