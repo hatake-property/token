bf9bf6fba7dfdcf29aa53751565abbe24dcc3432
hatake <hermit183@outlook.com>
2025-06-19 16:55:43 +0900

    fix bug

--- token.bash
+++ token.bash
@@ -3,22 +3,26 @@
 path=""
 
 if [ $# -eq 0 ]; then
-	for (( i=0; true; i++ )); do
+	i=1
+	while true; do
 		path="untitled-$i.txt"
-		if [ ! -f $path ]; then
+		if [ ! -f "$path" ]; then
 			break
 		fi
+		(( i++ ))
 	done
 else
 	path=$1
 fi
 
+echo "$path"
+
 cmd=""
 
 while true; do
 	read -s -n 1 key
-	cmd+=key
-	case cmd in
+	cmd+="$key"
+	case "$cmd" in
 		[Qq])
 			# Quit Program
 			break
